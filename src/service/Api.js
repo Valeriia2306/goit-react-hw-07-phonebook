b2ec7e95f9ed50d5c01961096ad2865bf10ec4a4
Valeriia Frolova <valeriiats1989@gmail.com>
2023-04-05 16:31:10 +0300

    add api

---
+++ src/service/Api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://642d7ab9bf8cbecdb4076786.mockapi.io';
+
+export const fetchContacts = async () => {
+  const response = await axios.get('/contacts');
+  return response.data;
+};
+
+export const addContact = async contact => {
+  const response = await axios.get('/contacts', contact);
+  return response.data;
+};
+
+export const deleteContact = async id => {
+  const response = await axios.get(`/contacts/${id}`);
+  return response.data.id;
+};
